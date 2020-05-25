73da63f57ad40a0ae9d4ca8c3fda51c429dc76e2
aberke <berke.alexandra@gmail.com>
2020-05-25 10:51:02 -0400

    updated plotting  of trajectory vectors

--- trajectory_synthesis/evaluation/eval_util.py
+++ trajectory_synthesis/evaluation/eval_util.py
@@ -4,7 +4,9 @@ Utilities for evaluation.
 
 import os
 
+from matplotlib.lines import Line2D
 import matplotlib.pyplot as plt
+
 import numpy as np
 import pandas as pd
 
@@ -41,43 +43,73 @@ def print_dwell_vector_by_days(v):
         print(day_chunk)
 
 
-def plot_trajectory_vector_frequencies(trajectory_vector, by_frequency_order=False):
-    """Plots the places the the person with the given trajectory has gone, by the frequency which
-        the person goes to them.
-        Each index is an hour. The value at that index represents where the user spent the most
-        time during that hour.
-        
-        args: (bool) when by_frequency_order is true, the frequency shown is relative
-            to the other places the user spent time.
-        
-        The null values that represent when the user location was
-            not recorded are set to frequency=0 in the plot.
-            
-        Returns dataframe representing trajectory frequencies.
-    """
-    _prefix, trajectory_vector = break_prefix(trajectory_vector)
-    df = pd.DataFrame({PLACE_LABEL:trajectory_vector})
+def plot_trajectory_vector_frequencies(trajectory_vector, by_frequency_order=False, show_null=False):
+    [home_label, work_label], unprefixed_t = break_prefix(trajectory_vector)
+    df = pd.DataFrame({PLACE_LABEL:unprefixed_t})
     labels = list(set(trajectory_vector))
-    labels_to_frequency = {label: df[df[PLACE_LABEL] == label].shape[0] for label in labels}
-    # Set null location labels to frequency=0
-    labels_to_frequency[0] = 0
+    labels_to_frequency = df[PLACE_LABEL].value_counts().to_dict()
+
+    if show_null:
+        # Set null location labels to frequency=0
+        labels_to_frequency[0] = 0
+    elif (0 in labels_to_frequency):
+        del labels_to_frequency[0]
 
     sorted_frequencies = sorted(labels_to_frequency.values())
     frequency_to_frequency_order = {sorted_frequencies[f]: f for f in range(len(sorted_frequencies))}
+
     df[PLACE_LABEL_BY_FREQUENCY] = df[PLACE_LABEL].map(labels_to_frequency)
     df[PLACE_LABEL_BY_FREQUENCY_ORDER] = df[PLACE_LABEL_BY_FREQUENCY].map(frequency_to_frequency_order)
-    
-    y = PLACE_LABEL_BY_FREQUENCY_ORDER if by_frequency_order else PLACE_LABEL_BY_FREQUENCY
-    title = 'Places visited per hour, over %s days, by frequency.  Total places visited: %s' % (DAYS, len(labels))
-    ax = df.plot(
-        y=y,
-        style='o',
-        figsize=(18, 4),
-        title=title,
+
+    y = df[PLACE_LABEL_BY_FREQUENCY_ORDER] if by_frequency_order else df[PLACE_LABEL_BY_FREQUENCY]
+
+    places_count = len(set(labels) - set([0]))
+    title = 'Places visited per hour, over %s days, by frequency. %s different places visited.' % (DAYS, places_count)
+    fig, ax = plt.subplots(1, figsize=(16, 4))
+    colors = get_plot_colors(home_label, unprefixed_t)
+    marker = 'o'
+    marker_size = 35
+    plt.rc('font', **{'family':'normal','weight':'regular','size': 12})
+    plt.scatter(
+        range(len(df)), # x
+        y,
+        c=colors,
+        s=marker_size, # marker size
+        marker=marker,
+    )
+    ax.set_title(title)
+    legend_elements = [
+        Line2D([0], [0], marker=marker, color=HOME_COLOR, label='Home', markerfacecolor=HOME_COLOR),
+        Line2D([0], [0], marker=marker, color=OTHER_PLACE_COLOR, label='Other place', markerfacecolor=OTHER_PLACE_COLOR),
+    ]
+    ax.legend(
+        handles=legend_elements, loc='upper right',
+        labelspacing=1,
+        markerscale=1.5,
+        # bbox (x, y, width, height)
+        bbox_to_anchor=(1.15, 1) #, 0, 0.1))
     )
     plt.xticks([24*i for i in range(DAYS)])
+    plt.grid(b=True,which='major',axis='x',color='gray',linestyle='--')
+    # ax.get_yaxis().set_ticks([]) # to turn off the numbers for frequency
+    plt.grid(False,axis='y')
+    ax.set_ylabel('Frequency')
+    ax.set_xlabel('Hour')
     plt.show()
+
     return df
 
 
+HOME_COLOR = 'purple'
+OTHER_PLACE_COLOR  = 'coral'
+
+def get_plot_colors(h_label, labels, h_color=HOME_COLOR, o_color=OTHER_PLACE_COLOR):
+    """
+    Helper function to plot_trajectory_vector_frequencies.
+    Returns list of colors corrsponding to list of labels.
+    """
+    # cast to same type, just in case
+    h_label, labels = int(h_label), [int(l) for l in labels]
+    colors = [h_color if int(l) == h_label  else o_color for l in labels]
+    return colors
 
